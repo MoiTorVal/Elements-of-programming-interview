11b1cecacd7611e9899e32687293416fc19db168
moises valdez <moisesvaldez0559@gmail.com>
2025-02-08 09:00:31 -0800

    palindrome

---
+++ palindrome.py
@@ -0,0 +1,25 @@
+import math 
+# book
+def isPalindrom(x):
+    if x <= 0:
+        return x == 0
+    
+    num_digits = math.floor(math.log10(x)) + 1
+    msd_mask = 10**(num_digits - 1)
+    for i in range(num_digits // 2):
+        if x // msd_mask != x % 10:
+            return False
+        x %= msd_mask 
+        x //= 10 
+        msd_mask //= 100
+    return True
+
+# deepseek
+def isPalindrome(x):
+    if x < 0:
+        return False
+    
+    num_str = str(x)
+
+    return num_str == num_str[::-1]
+
