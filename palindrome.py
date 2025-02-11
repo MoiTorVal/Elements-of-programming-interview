51ac4ea6c2dbfe78044a1f55ecf4ad166275e86c
moises valdez <moisesvaldez0559@gmail.com>
2025-02-11 10:46:37 -0800

    palindome.py understanding the math

--- palindrome.py
+++ palindrome.py
@@ -1,25 +1,28 @@
 import math 
-# book
-def isPalindrom(x):
+# # book
+def isPalindrome(x):
     if x <= 0:
         return x == 0
     
     num_digits = math.floor(math.log10(x)) + 1
     msd_mask = 10**(num_digits - 1)
+
     for i in range(num_digits // 2):
         if x // msd_mask != x % 10:
             return False
         x %= msd_mask 
         x //= 10 
         msd_mask //= 100
+
     return True
 
 # deepseek
-def isPalindrome(x):
-    if x < 0:
-        return False
+# def isPalindrome(x):
+#     if x < 0:
+#         return False
     
-    num_str = str(x)
+#     num_str = str(x)
+    
+#     return num_str == num_str[::-1]
 
-    return num_str == num_str[::-1]
-
+print(isPalindrome(12321))
